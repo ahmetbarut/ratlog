71526564411a6b7c7bb264fc38cd2cf479ff7d8d
Ahmet Barut <ahmetbarut588@gmail.com>
2026-02-21 23:56:41 +0300

    Refactor version handling in cli.rs for improved clarity
    - Updated the version constant in cli.rs to use a match statement for better readability and to handle the case where the RATLOG_VERSION environment variable is not set, falling back to the Cargo package version.

--- src/cli.rs
+++ src/cli.rs
@@ -4,8 +4,10 @@ use std::path::PathBuf;
 
 use crate::constants::MAX_LINES;
 
-const VERSION: &str =
-    option_env!("RATLOG_VERSION").unwrap_or(env!("CARGO_PKG_VERSION"));
+const VERSION: &str = match option_env!("RATLOG_VERSION") {
+    Some(v) => v,
+    None => env!("CARGO_PKG_VERSION"),
+};
 
 #[derive(Debug)]
 pub enum CliAction {
